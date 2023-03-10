6bdf20ba66d4f715a845f7b8de36ce7b110ea041
avocadocoder73 <113757969+avocadocoder73@users.noreply.github.com>
2023-03-09 19:35:13 -0600

    LATEST

---
+++ build/precache-manifest.0c1c1254b41b025fc1797d8715c24b74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f931d9714e5292a6d18e",
+    "url": "/Tweetle/static/css/main.85b0f557.chunk.css"
+  },
+  {
+    "revision": "f931d9714e5292a6d18e",
+    "url": "/Tweetle/static/js/main.c633891b.chunk.js"
+  },
+  {
+    "revision": "cbfba2bd6a5f77545ecb",
+    "url": "/Tweetle/static/js/runtime~main.da6ab633.js"
+  },
+  {
+    "revision": "9e975a77631faebbba72",
+    "url": "/Tweetle/static/js/2.4c6e1d06.chunk.js"
+  },
+  {
+    "revision": "3eb3f95558cd90c6a033",
+    "url": "/Tweetle/static/js/3.f03f87d3.chunk.js"
+  },
+  {
+    "revision": "7f3455940bedf033be4f386fc4762378",
+    "url": "/Tweetle/static/media/kyliejennernotcorrect.7f345594.png"
+  },
+  {
+    "revision": "96a4dfba87a5aaddb027d856175aebc5",
+    "url": "/Tweetle/static/media/kyliejennercorrect.96a4dfba.png"
+  },
+  {
+    "revision": "ed8afc849ce7566dd175d4bcb9563771",
+    "url": "/Tweetle/index.html"
+  }
+];
